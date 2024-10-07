75e85a835cfc1d443e2891d37d76b44f475a1371
William Onyango <ojwangwiliam@gmail.com>
2024-10-07 11:47:22 +0300

    Add project files.

---
+++ Firstapp/Form1.Designer.vb
@@ -0,0 +1,389 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        btnHide = New Button()
+        lblProgramming = New Label()
+        lblName = New Label()
+        txtName = New TextBox()
+        btnShow = New Button()
+        btnColor = New Button()
+        btnEraseTxt = New Button()
+        btnSize22 = New Button()
+        btnExit = New Button()
+        btnSize30 = New Button()
+        btnSize40 = New Button()
+        btnItalic = New Button()
+        btnUnderline = New Button()
+        lblDOB = New Label()
+        RadioButton1 = New RadioButton()
+        GroupBox1 = New GroupBox()
+        RadioButton2 = New RadioButton()
+        units = New GroupBox()
+        CheckBox4 = New CheckBox()
+        CheckBox3 = New CheckBox()
+        CheckBox2 = New CheckBox()
+        CheckBox1 = New CheckBox()
+        Hobbies = New ListBox()
+        lblHobies = New Label()
+        txtHobies = New TextBox()
+        btnAdd = New Button()
+        DateTimePicker1 = New DateTimePicker()
+        btnSubmit = New Button()
+        GroupBox1.SuspendLayout()
+        units.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' btnHide
+        ' 
+        btnHide.Location = New Point(105, 257)
+        btnHide.Name = "btnHide"
+        btnHide.Size = New Size(75, 23)
+        btnHide.TabIndex = 0
+        btnHide.Text = "Hide"
+        btnHide.UseVisualStyleBackColor = True
+        ' 
+        ' lblProgramming
+        ' 
+        lblProgramming.AutoSize = True
+        lblProgramming.Location = New Point(12, 25)
+        lblProgramming.Name = "lblProgramming"
+        lblProgramming.Size = New Size(181, 15)
+        lblProgramming.TabIndex = 1
+        lblProgramming.Text = "Welcome to visual programming"
+        ' 
+        ' lblName
+        ' 
+        lblName.AutoSize = True
+        lblName.Location = New Point(12, 58)
+        lblName.Name = "lblName"
+        lblName.Size = New Size(68, 15)
+        lblName.TabIndex = 2
+        lblName.Text = "My name is"
+        ' 
+        ' txtName
+        ' 
+        txtName.Location = New Point(193, 55)
+        txtName.Name = "txtName"
+        txtName.Size = New Size(100, 23)
+        txtName.TabIndex = 3
+        ' 
+        ' btnShow
+        ' 
+        btnShow.Location = New Point(232, 257)
+        btnShow.Name = "btnShow"
+        btnShow.Size = New Size(75, 23)
+        btnShow.TabIndex = 4
+        btnShow.Text = "Show"
+        btnShow.UseVisualStyleBackColor = True
+        ' 
+        ' btnColor
+        ' 
+        btnColor.Location = New Point(105, 286)
+        btnColor.Name = "btnColor"
+        btnColor.Size = New Size(75, 23)
+        btnColor.TabIndex = 5
+        btnColor.Text = "Colour"
+        btnColor.UseVisualStyleBackColor = True
+        ' 
+        ' btnEraseTxt
+        ' 
+        btnEraseTxt.Location = New Point(232, 286)
+        btnEraseTxt.Name = "btnEraseTxt"
+        btnEraseTxt.Size = New Size(75, 23)
+        btnEraseTxt.TabIndex = 6
+        btnEraseTxt.Text = "Erase Text"
+        btnEraseTxt.UseVisualStyleBackColor = True
+        ' 
+        ' btnSize22
+        ' 
+        btnSize22.Location = New Point(105, 315)
+        btnSize22.Name = "btnSize22"
+        btnSize22.Size = New Size(75, 23)
+        btnSize22.TabIndex = 7
+        btnSize22.Text = "Size 22"
+        btnSize22.UseVisualStyleBackColor = True
+        ' 
+        ' btnExit
+        ' 
+        btnExit.Location = New Point(232, 315)
+        btnExit.Name = "btnExit"
+        btnExit.Size = New Size(75, 23)
+        btnExit.TabIndex = 8
+        btnExit.Text = "Exit"
+        btnExit.UseVisualStyleBackColor = True
+        ' 
+        ' btnSize30
+        ' 
+        btnSize30.Location = New Point(105, 348)
+        btnSize30.Name = "btnSize30"
+        btnSize30.Size = New Size(75, 23)
+        btnSize30.TabIndex = 9
+        btnSize30.Text = "size 30"
+        btnSize30.UseVisualStyleBackColor = True
+        ' 
+        ' btnSize40
+        ' 
+        btnSize40.Location = New Point(105, 376)
+        btnSize40.Name = "btnSize40"
+        btnSize40.Size = New Size(75, 23)
+        btnSize40.TabIndex = 10
+        btnSize40.Text = "size 40"
+        btnSize40.UseVisualStyleBackColor = True
+        ' 
+        ' btnItalic
+        ' 
+        btnItalic.Location = New Point(232, 376)
+        btnItalic.Name = "btnItalic"
+        btnItalic.Size = New Size(75, 23)
+        btnItalic.TabIndex = 11
+        btnItalic.Text = "Italics"
+        btnItalic.UseVisualStyleBackColor = True
+        ' 
+        ' btnUnderline
+        ' 
+        btnUnderline.Location = New Point(232, 348)
+        btnUnderline.Name = "btnUnderline"
+        btnUnderline.Size = New Size(75, 23)
+        btnUnderline.TabIndex = 12
+        btnUnderline.Text = "Underline"
+        btnUnderline.UseVisualStyleBackColor = True
+        ' 
+        ' lblDOB
+        ' 
+        lblDOB.AutoSize = True
+        lblDOB.Location = New Point(12, 85)
+        lblDOB.Name = "lblDOB"
+        lblDOB.Size = New Size(73, 15)
+        lblDOB.TabIndex = 13
+        lblDOB.Text = "Date of birth"
+        ' 
+        ' RadioButton1
+        ' 
+        RadioButton1.AutoSize = True
+        RadioButton1.Location = New Point(88, 12)
+        RadioButton1.Name = "RadioButton1"
+        RadioButton1.Size = New Size(51, 19)
+        RadioButton1.TabIndex = 16
+        RadioButton1.TabStop = True
+        RadioButton1.Text = "male"
+        RadioButton1.UseVisualStyleBackColor = True
+        ' 
+        ' GroupBox1
+        ' 
+        GroupBox1.Controls.Add(RadioButton2)
+        GroupBox1.Controls.Add(RadioButton1)
+        GroupBox1.Location = New Point(12, 114)
+        GroupBox1.Name = "GroupBox1"
+        GroupBox1.Size = New Size(237, 38)
+        GroupBox1.TabIndex = 17
+        GroupBox1.TabStop = False
+        GroupBox1.Text = "Gender"
+        ' 
+        ' RadioButton2
+        ' 
+        RadioButton2.AutoSize = True
+        RadioButton2.Location = New Point(155, 12)
+        RadioButton2.Name = "RadioButton2"
+        RadioButton2.Size = New Size(61, 19)
+        RadioButton2.TabIndex = 17
+        RadioButton2.TabStop = True
+        RadioButton2.Text = "female"
+        RadioButton2.UseVisualStyleBackColor = True
+        ' 
+        ' units
+        ' 
+        units.Controls.Add(CheckBox4)
+        units.Controls.Add(CheckBox3)
+        units.Controls.Add(CheckBox2)
+        units.Controls.Add(CheckBox1)
+        units.ForeColor = Color.Black
+        units.Location = New Point(12, 158)
+        units.Name = "units"
+        units.Size = New Size(286, 83)
+        units.TabIndex = 18
+        units.TabStop = False
+        units.Text = "Register units"
+        ' 
+        ' CheckBox4
+        ' 
+        CheckBox4.AutoSize = True
+        CheckBox4.ForeColor = Color.Black
+        CheckBox4.Location = New Point(109, 47)
+        CheckBox4.Name = "CheckBox4"
+        CheckBox4.Size = New Size(164, 19)
+        CheckBox4.TabIndex = 3
+        CheckBox4.Text = "Application programming"
+        CheckBox4.UseVisualStyleBackColor = True
+        ' 
+        ' CheckBox3
+        ' 
+        CheckBox3.AutoSize = True
+        CheckBox3.ForeColor = Color.Black
+        CheckBox3.Location = New Point(21, 47)
+        CheckBox3.Name = "CheckBox3"
+        CheckBox3.Size = New Size(48, 19)
+        CheckBox3.TabIndex = 2
+        CheckBox3.Text = "DSD"
+        CheckBox3.UseVisualStyleBackColor = True
+        ' 
+        ' CheckBox2
+        ' 
+        CheckBox2.AutoSize = True
+        CheckBox2.ForeColor = Color.Black
+        CheckBox2.Location = New Point(109, 22)
+        CheckBox2.Name = "CheckBox2"
+        CheckBox2.Size = New Size(71, 19)
+        CheckBox2.TabIndex = 1
+        CheckBox2.Text = "Calculus"
+        CheckBox2.UseVisualStyleBackColor = True
+        ' 
+        ' CheckBox1
+        ' 
+        CheckBox1.AutoSize = True
+        CheckBox1.ForeColor = Color.Black
+        CheckBox1.Location = New Point(21, 22)
+        CheckBox1.Name = "CheckBox1"
+        CheckBox1.Size = New Size(75, 19)
+        CheckBox1.TabIndex = 0
+        CheckBox1.Text = "Control 1"
+        CheckBox1.UseVisualStyleBackColor = True
+        ' 
+        ' Hobbies
+        ' 
+        Hobbies.ForeColor = SystemColors.MenuHighlight
+        Hobbies.FormattingEnabled = True
+        Hobbies.ItemHeight = 15
+        Hobbies.Location = New Point(469, 58)
+        Hobbies.Name = "Hobbies"
+        Hobbies.Size = New Size(286, 169)
+        Hobbies.TabIndex = 19
+        ' 
+        ' lblHobies
+        ' 
+        lblHobies.AutoSize = True
+        lblHobies.Location = New Point(414, 29)
+        lblHobies.Name = "lblHobies"
+        lblHobies.Size = New Size(126, 15)
+        lblHobies.TabIndex = 20
+        lblHobies.Text = "What are your hobbies"
+        ' 
+        ' txtHobies
+        ' 
+        txtHobies.Location = New Point(546, 25)
+        txtHobies.Name = "txtHobies"
+        txtHobies.Size = New Size(144, 23)
+        txtHobies.TabIndex = 21
+        ' 
+        ' btnAdd
+        ' 
+        btnAdd.Location = New Point(696, 25)
+        btnAdd.Name = "btnAdd"
+        btnAdd.Size = New Size(75, 23)
+        btnAdd.TabIndex = 22
+        btnAdd.Text = "Add"
+        btnAdd.UseVisualStyleBackColor = True
+        ' 
+        ' DateTimePicker1
+        ' 
+        DateTimePicker1.Location = New Point(193, 85)
+        DateTimePicker1.Name = "DateTimePicker1"
+        DateTimePicker1.Size = New Size(200, 23)
+        DateTimePicker1.TabIndex = 23
+        ' 
+        ' btnSubmit
+        ' 
+        btnSubmit.Location = New Point(548, 276)
+        btnSubmit.Name = "btnSubmit"
+        btnSubmit.Size = New Size(124, 52)
+        btnSubmit.TabIndex = 24
+        btnSubmit.Text = "SUBMIT"
+        btnSubmit.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(784, 461)
+        Controls.Add(btnSubmit)
+        Controls.Add(DateTimePicker1)
+        Controls.Add(btnAdd)
+        Controls.Add(txtHobies)
+        Controls.Add(lblHobies)
+        Controls.Add(Hobbies)
+        Controls.Add(units)
+        Controls.Add(GroupBox1)
+        Controls.Add(lblDOB)
+        Controls.Add(btnUnderline)
+        Controls.Add(btnItalic)
+        Controls.Add(btnSize40)
+        Controls.Add(btnSize30)
+        Controls.Add(btnExit)
+        Controls.Add(btnSize22)
+        Controls.Add(btnEraseTxt)
+        Controls.Add(btnColor)
+        Controls.Add(btnShow)
+        Controls.Add(txtName)
+        Controls.Add(lblName)
+        Controls.Add(lblProgramming)
+        Controls.Add(btnHide)
+        Name = "Form1"
+        Text = "Form1"
+        GroupBox1.ResumeLayout(False)
+        GroupBox1.PerformLayout()
+        units.ResumeLayout(False)
+        units.PerformLayout()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents btnHide As Button
+    Friend WithEvents lblProgramming As Label
+    Friend WithEvents lblName As Label
+    Friend WithEvents txtName As TextBox
+    Friend WithEvents btnShow As Button
+    Friend WithEvents btnColor As Button
+    Friend WithEvents btnEraseTxt As Button
+    Friend WithEvents btnSize22 As Button
+    Friend WithEvents btnExit As Button
+    Friend WithEvents btnSize30 As Button
+    Friend WithEvents btnSize40 As Button
+    Friend WithEvents btnItalic As Button
+    Friend WithEvents btnUnderline As Button
+    Friend WithEvents lblDOB As Label
+    Friend WithEvents RadioButton1 As RadioButton
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents RadioButton2 As RadioButton
+    Friend WithEvents units As GroupBox
+    Friend WithEvents CheckBox4 As CheckBox
+    Friend WithEvents CheckBox3 As CheckBox
+    Friend WithEvents CheckBox2 As CheckBox
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents Hobbies As ListBox
+    Friend WithEvents lblHobies As Label
+    Friend WithEvents txtHobies As TextBox
+    Friend WithEvents btnAdd As Button
+    Friend WithEvents DateTimePicker1 As DateTimePicker
+    Friend WithEvents btnSubmit As Button
+
+End Class
---
+++ Firstapp/Form1.vb
@@ -0,0 +1,109 @@
+﻿Public Class Form1
+    Private Sub btnOnclick_Click(sender As Object, e As EventArgs) Handles btnHide.Click
+        lblName.Visible = False
+        lblProgramming.Visible = False
+        txtName.Visible = False
+        lblDOB.Visible = False
+        DateTimePicker1.Visible = False
+        GroupBox1.Visible = False
+        units.Visible = False
+
+
+    End Sub
+
+    Private Sub btnShow_Click(sender As Object, e As EventArgs) Handles btnShow.Click
+        lblName.Visible = True
+        lblProgramming.Visible = True
+        txtName.Visible = True
+        lblDOB.Visible = True
+        DateTimePicker1.Visible = True
+        GroupBox1.Visible = True
+        units.Visible = True
+    End Sub
+
+    Private Sub btnColor_Click(sender As Object, e As EventArgs) Handles btnColor.Click
+        lblName.ForeColor = Color.White
+        lblProgramming.ForeColor = Color.Wheat
+        lblName.BackColor = Color.Red
+        lblProgramming.BackColor = Color.Red
+    End Sub
+
+    Private Sub btnEraseTxt_Click(sender As Object, e As EventArgs) Handles btnEraseTxt.Click
+        txtName.Clear()
+
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+
+    Private Sub btnSize_Click(sender As Object, e As EventArgs) Handles btnSize22.Click
+        lblName.Font = New Font(Font.Size, 22)
+        lblProgramming.Font = New Font(Font.Size, 22)
+        txtName.Font = New Font(Font.Size, 22)
+    End Sub
+
+    Private Sub btnSize30_Click(sender As Object, e As EventArgs) Handles btnSize30.Click
+        lblName.Font = New Font(Font.Size, 30)
+        lblProgramming.Font = New Font(Font.Size, 30)
+        txtName.Font = New Font(Font.Size, 30)
+    End Sub
+
+    Private Sub btnSize40_Click(sender As Object, e As EventArgs) Handles btnSize40.Click
+        lblName.Font = New Font(Font.Size, 40)
+        lblProgramming.Font = New Font(Font.Size, 40)
+        txtName.Font = New Font(Font.Size, 40)
+    End Sub
+
+    Private Sub btnUnderline_Click(sender As Object, e As EventArgs) Handles btnUnderline.Click
+        lblName.Font = New Font(lblName.Font.FontFamily, 16, FontStyle.Underline)
+        lblProgramming.Font = New Font(lblProgramming.Font.FontFamily, 16, FontStyle.Underline)
+    End Sub
+
+    Private Sub btnItalic_Click(sender As Object, e As EventArgs) Handles btnItalic.Click
+        lblName.Font = New Font(lblName.Font.FontFamily, 16, FontStyle.Italic)
+        lblProgramming.Font = New Font(lblProgramming.Font.FontFamily, 16, FontStyle.Italic)
+    End Sub
+
+    Private Sub btnAdd_Click(sender As Object, e As EventArgs) Handles btnAdd.Click
+        Hobbies.Items.Add(txtHobies.Text)
+        txtHobies.Clear()
+    End Sub
+
+    Private Sub btnSubmit_Click(sender As Object, e As EventArgs) Handles btnSubmit.Click
+        ' Get all hobbies from ListBox (Hobbies)
+        Dim allHobbies As String = ""
+        For Each hobby As String In Hobbies.Items
+            allHobbies &= hobby & ", "
+        Next
+        ' Remove the trailing comma and space
+        If allHobbies.Length > 0 Then
+            allHobbies = allHobbies.Substring(0, allHobbies.Length - 2)
+        End If
+
+        ' Get selected units from CheckBoxes
+        Dim selectedUnits As String = ""
+        If CheckBox1.Checked Then
+            selectedUnits &= CheckBox1.Text & ", "
+        End If
+        If CheckBox2.Checked Then
+            selectedUnits &= CheckBox2.Text & ", "
+        End If
+        If CheckBox3.Checked Then
+            selectedUnits &= CheckBox3.Text & ", "
+        End If
+        If CheckBox4.Checked Then
+            selectedUnits &= CheckBox4.Text & ", "
+        End If
+        ' Remove the trailing comma and space
+        If selectedUnits.Length > 0 Then
+            selectedUnits = selectedUnits.Substring(0, selectedUnits.Length - 2)
+        End If
+
+        ' Display the message
+        MessageBox.Show("Welcome " & txtName.Text & ", you were born in " & DateTimePicker1.Value.Year.ToString() & ". " &
+                        "You are registered to do the following units: " & selectedUnits & " and your hobbies are: " & allHobbies)
+    End Sub
+
+
+End Class
